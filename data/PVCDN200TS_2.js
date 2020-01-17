0faff924deee52d90e5dcc2cb1ef499a69e31160
Tegar Pualam Syuhada <tegarpualamsyuhada@gmail.com>
2020-01-17 16:44:10 +0700

    Add files via upload

---
+++ data/PVCDN200TS_2.js
@@ -0,0 +1,9 @@
+var json_PVCDN200TS_2 = {
+"type": "FeatureCollection",
+"name": "PVCDN200TS_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ELEVATION": 2.069, "material": "PVC", "diameter": "200", "panjang": 182.61 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.411256561983848, -5.162610078126545 ], [ 119.411140692624457, -5.163064086572116 ], [ 119.411047860317083, -5.1634283080205 ], [ 119.41095692046018, -5.163801877309966 ], [ 119.410855416439745, -5.164211668474977 ] ] ] } },
+{ "type": "Feature", "properties": { "ELEVATION": 1.473, "material": "PVC", "diameter": "200", "panjang": 120.669 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 119.408556932638007, -5.162712893227512 ], [ 119.408668842711506, -5.162517719719996 ], [ 119.408889219606579, -5.162121832466761 ], [ 119.409091877423123, -5.16176260822117 ] ] ] } }
+]
+}
